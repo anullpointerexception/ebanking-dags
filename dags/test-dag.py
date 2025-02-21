035d251687d0182470fc62e29f7a73a63f4c5968
Maximilian Turza <maxturza@hotmail.com>
2025-02-21 13:31:30 +0100

    upload first dag

---
+++ dags/test-dag.py
@@ -0,0 +1,27 @@
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from datetime import datetime
+
+def print_hello():
+    print("Hello, World!")
+
+default_args = {
+    'owner': 'airflow',
+    'start_date': datetime(2024, 2, 21),
+    'catchup': False
+}
+
+with DAG(
+    'hello_world_dag',
+    default_args=default_args,
+    schedule_interval='@daily',
+    description='A simple DAG that prints Hello World',
+    tags=['example']
+) as dag:
+
+    task_hello = PythonOperator(
+        task_id='print_hello',
+        python_callable=print_hello
+    )
+
+    task_hello
